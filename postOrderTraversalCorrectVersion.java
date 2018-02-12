191b4cfa05c379efa7a608cfa52af0dd425f5284
adalucky1234 <35015656+adalucky1234@users.noreply.github.com>
2018-02-11 22:34:26 -0800

    Update postOrderTraversalCorrectVersion.java

--- postOrderTraversalCorrectVersion.java
+++ postOrderTraversalCorrectVersion.java
@@ -1,6 +1,17 @@
 /**
-for this version, we implement post Order Traversal of tree using one stack, simply just recursively push current root node, 
-then push its right subtree, then recursively push its left subtree.
+for this version, we implement post Order Traversal of tree using one stack, simply just push current root node, 
+then recursively push its right subtree, then recursively push its left subtree.
+
+For this example tree, we get the expected result: 6 9 2 7 8 4 5 3 1 
+
+      1
+    /  \
+   2    3
+ /  \   / \
+6   9   4  5
+    / \
+   7   8
+
 */
 
 
@@ -19,7 +30,6 @@ public class postOrderTraversalCorrectVersion {
         root.right.left = new TreeNode(4);
         root.right.left.left = new TreeNode(7);
         root.right.left.right = new TreeNode(8);
-        //ArrayList<Integer> list = s.StackList(root);
         solution.postOrderTraversal(root);
         while (!solution.s.isEmpty()) {
             TreeNode t = solution.s.pop();
@@ -42,12 +52,12 @@ class Solution {
 
     public void postOrderTraversal(TreeNode root) {
         if(root == null) return;
-        s.push(root);
+        s.push(root); // push root first, guarentee it's been poped out lastly
         if (root.right != null) {
-            postOrderTraversal(root.right);
+            postOrderTraversal(root.right); // if has right child, then recursively push into stack
         }
         if (root.left != null) {
-            postOrderTraversal(root.left);
+            postOrderTraversal(root.left); // if has left child, then recursively push into stack
         }
     }
 }
