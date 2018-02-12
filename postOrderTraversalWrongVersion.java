7e9f143e1e857dfb3a19ed49c0a679159211a6ab
adalucky1234 <35015656+adalucky1234@users.noreply.github.com>
2018-02-11 22:26:29 -0800

    Update and rename postOrderTraversal.java to postOrderTraversalWrongVersion.java

--- postOrderTraversalWrongVersion.java
+++ postOrderTraversalWrongVersion.java
@@ -1,32 +1,39 @@
 /* This algorithm is to implement postOrder traversal of a tree using one stack. 
-First, push root into stack, mark it as curcode;
+First, push root into stack, mark it as cur node;
 
-Then, check if cur has right child, if does, then push its right child, mark cur as parent node, update cur node's right 
-child to cur node, continue this loop until cur doesn't have a right child;
+Then, check if cur has right child, if does, then push its right child, mark cur as parent node, update cur's right 
+child to cur, repeat this process until cur doesn't have a right child;
 
-Then go back to parent node, if parent exits and parent has left child, then recursively call the algorithm, pass parent's 
+Then go back to parent node, if parent exits and parent has left child, then recursively call the method, pass parent's 
 left child as parameter.
 
 Finally, the stack is built up.
 
-The example tree is as following: the expected printed results is 7 8 4 5 3 1 
-     1
-    / \
-   2   3
-      / \
-     4   5
+The example tree is as following: the expected printed results is 6 9 2 7 8 4 5 3 1 , but actually the result is 7 8 4 5 3 1 
+
+Why? The reson is though we can correctly traverse the right subtree, we didn't get back from the last parent(7) to the original
+root node(1), so we couldn't use this marking method to traverse its left subtree. Based on this, I wrote a simplifed version.
+
+
+      1
+    /  \
+   2    3
+ /  \   / \
+6   9   4  5
     / \
    7   8
 
 */        
          
-public class postOrderTraversal {
+public class postOrderTraversalWrongVersion {
 
     public static void main(String[] args) {
         // write your code here
         Solution s = new Solution();
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
+        root.left.left = new TreeNode(6);
+        root.left.right = new TreeNode(9);
         root.right = new TreeNode(3);
         root.right.right = new TreeNode(5);
         root.right.left = new TreeNode(4);
